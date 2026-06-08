0b3163f14b51d043d1ac9a41a70756911df4e7bd
aashnimanasa-cloud <aashnimanasa@gmail.com>
2026-06-08 15:51:44 +0530

    added css and javascript

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function addTask(){
 
+    let input = document.getElementById("taskInput");
+
+    let task = input.value;
+
+    if(task === ""){
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        ${task}
+        <button onclick="this.parentElement.remove()">
+            Delete
+        </button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
